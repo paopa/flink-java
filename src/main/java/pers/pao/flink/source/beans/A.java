dd4f31b5cc90181f266229c2654241efa23ea79e
Pao-Sheng Wang <david840422@gmail.com>
2021-02-05 01:07:27 +0800

    source api test

---
+++ src/main/java/pers/pao/flink/source/beans/A.java
@@ -0,0 +1,22 @@
+package pers.pao.flink.source.beans;
+
+import lombok.Data;
+
+/**
+ * 範例物件
+ */
+@Data
+public class A {
+    private final String id;
+    private final long num1;
+    private final Double num2;
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "id='" + id + '\'' +
+                ", num1=" + num1 +
+                ", num2=" + num2 +
+                '}';
+    }
+}
---
+++ src/main/java/pers/pao/flink/source/collection/Source.java
@@ -0,0 +1,33 @@
+package pers.pao.flink.source.collection;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import pers.pao.flink.source.beans.A;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Source {
+    private final static List<A> list = Arrays.asList(
+            new A("id1", 100, 0.1),
+            new A("id2", 200, 0.2),
+            new A("id3", 300, 0.3),
+            new A("id4", 400, 0.4)
+    );
+
+    public static List<A> get() {
+        return list;
+    }
+}
+
+class Test {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        env.setParallelism(1);
+        DataStream<A> stream = env.fromCollection(Source.get());
+        DataStream<Integer> intStream = env.fromElements(1, 2, 3, 4, 5);
+        stream.print("A");
+        intStream.print("int");
+        env.execute();
+    }
+}
